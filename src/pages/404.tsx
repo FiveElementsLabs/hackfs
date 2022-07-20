4476b8e225e1d80a771e266074adf312f50cbc87
Nicolas <nicolas@racchi.com>
2022-07-20 11:41:07 +0300

    feat: 404 fixes

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,23 +4,21 @@ import React from "react";
 export default function Custom404() {
   return (
     <>
-      <div className="max-w-7xl mx-auto px-4 py-16 text-center sm:px-6 sm:py-24 lg:px-8 lg:py-48">
-        <p className="text-sm font-semibold text-tide-lighter text-opacity-50 uppercase tracking-wide">
+      <div className="max-w-4xl mx-auto px-4 py-16 text-center sm:px-6 sm:py-24 lg:px-8 lg:py-48">
+        <p className="text-sm font-semibold text-tide-lighter opacity-70 uppercase tracking-wide">
           404 error
         </p>
-        <h1 className="mt-2 text-4xl font-extrabold text-white tracking-tight sm:text-5xl">
-          Oh! You've been caught by the tide.
+        <h1 className="mt-2 text-4xl font-extrabold tracking-tight">
+          Oh! You&apos;ve been caught by the tide.
         </h1>
-        <p className="mt-2 text-lg font-medium text-tide-lighter text-opacity-50">
-          It looks like the page you're looking for doesn't exist.
+        <p className="mt-2 text-lg font-medium text-tide-lighter opacity-70">
+          It looks like the page you&apos;re looking for doesn&apos;t exist.
         </p>
-        <div className="mt-6">
-          <Link
-            href="/"
-            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-black text-opacity-75 bg-white bg-opacity-75 sm:bg-opacity-25 sm:hover:bg-opacity-50"
-          >
-            <a>Go back home</a>
-          </Link>
+        <div
+          className="mt-8 inline-flex items-center px-8 py-2 border border-transparent text-sm 
+            font-medium rounded-md bg-elements hover:bg-opacity-50"
+        >
+          <Link href="/">Go back home</Link>
         </div>
       </div>
     </>
