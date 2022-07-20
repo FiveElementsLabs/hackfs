584493671789f84dd4d6abb1ddcb9d0eda1fee44
Christian Kobril <christiankobril@gmail.com>
2022-07-20 11:45:45 +0300

    feat: change layout of active campaigns

--- src/components/active-campaign/active-campaign-view.tsx
+++ src/components/active-campaign/active-campaign-view.tsx
@@ -1,14 +1,48 @@
 import React, { useState } from "react";
 import ActiveCampaignsBox from "./active-campaign-box";
 import { ActiveCampaignsBoxEmpty } from "./active-campaign-box";
+import { getDeadline } from "../../utils/utils";
 
-const campaigns = [];
+const campaigns = [
+  {
+    title: "Token Giveaway",
+    imageURL:
+      "https://img.freepik.com/free-photo/medical-pill-with-bitcoin-sign-pink-background-bitcoin-technology-concept-medicine_535844-1506.jpg?w=2000",
+    deadline: getDeadline(new Date(new Date().setDate(new Date().getDate() + 7))).toString(),
+    participants: "21",
+    drawMethod: "0",
+  },
+  {
+    title: "5EL NFT Giveaway",
+    imageURL:
+      "https://d33wubrfki0l68.cloudfront.net/13ca0c32ffd56bcfaf861b9a8acb212d0f6482e3/d8df6/static/c3bcc8c47890ffd2a2c329972c73d0fd/e018d/ethereum-logo-portrait-black-gray.png",
+    deadline: getDeadline(new Date(new Date().setDate(new Date().getDate() + 6))).toString(),
+    participants: "189",
+    drawMethod: "0",
+  },
+  {
+    title: "Token Giveaway #2",
+    imageURL:
+      "https://images.cointelegraph.com/images/1434_aHR0cHM6Ly9zMy5jb2ludGVsZWdyYXBoLmNvbS91cGxvYWRzLzIwMjEtMDQvNTg3NTgxZjItNjE3Yi00MDRiLWIzNTgtOGI0NjM2ZDQzOTRiLmpwZw==.jpg",
+    deadline: getDeadline(new Date(new Date().setDate(new Date().getDate() + 5))).toString(),
+    participants: "12",
+    drawMethod: "0",
+  },
+  {
+    title: "5EL NFT Giveaway",
+    imageURL:
+      "https://d33wubrfki0l68.cloudfront.net/13ca0c32ffd56bcfaf861b9a8acb212d0f6482e3/d8df6/static/c3bcc8c47890ffd2a2c329972c73d0fd/e018d/ethereum-logo-portrait-black-gray.png",
+    deadline: getDeadline(new Date(new Date().setDate(new Date().getDate() + 7))).toString(),
+    participants: "189",
+    drawMethod: "0",
+  },
+];
 
 const ActiveCampaignsView = () => {
   const [switchCampaign, setSwitchCampaign] = useState(true);
 
   return (
-    <div className="grid place-content-center">
+    <div className="grid max-w-4-xl mx-auto">
       <div className="w-auto mx-auto relative py-8">
         <div className="flex">
           <button
