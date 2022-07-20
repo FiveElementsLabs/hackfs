3c3e4afc04c8f08b2cb63459b858969f1bcfe4e7
alby.giaco <alby.giaco@live.it>
2022-07-20 09:56:58 +0300

    first footer

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,49 @@
+import Tide from "../../assets/svg/tide";
+import FelFooter from "../../assets/svg/felFooter";
+import DocsFooter from "../../assets/svg/docsfooter";
+import ContactFooter from "../../assets/svg/contactfooter";
+import MediumFooter from "../../assets/svg/mediumfooter";
+import TwitterFooter from "../../assets/svg/twitterfooter";
+import DiscordFooter from "../../assets/svg/discordfooter";
+
+export default function Footer() {
+  return (
+    <>
+      <div className="pt-4 max-w-4xl mx-auto">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-7">
+            <div className="scale-75">
+              <Tide />
+            </div>
+
+            <div className="flex items-center">
+              {" "}
+              <FelFooter className="mr-2" />
+              By Five Elements Labs
+            </div>
+            <div className="flex items-center">
+              {" "}
+              <ContactFooter className="mr-2" />
+              Contact us
+            </div>
+            <div className="flex items-center">
+              {" "}
+              <DocsFooter className="mr-2" />
+              Docs
+            </div>
+            <div className="flex items-center">
+              {" "}
+              <MediumFooter className="mr-2" />
+              Blog
+            </div>
+          </div>
+          <div className="flex gap-3">
+            <TwitterFooter />
+            <DiscordFooter />
+            <TwitterFooter />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
