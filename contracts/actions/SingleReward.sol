13e2f55d1d26138727542319a328e60402b098c6
RiccardoGalbusera <93911514+RiccardoGalbusera@users.noreply.github.com>
2022-07-20 17:17:07 +0200

    single reward action

---
+++ contracts/actions/SingleReward.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0
+pragma solidity ^0.8.0;
+pragma abicoder v2;
+
+import "../CampaignStorage.sol";
+
+contract SingleRewardAction {
+  function claim() public {
+    StorageStruct storage Storage = CampaignStorage.getStorage();
+    IERC20 token = IERC20(Storage.rewardToken);
+    require(
+      Storage.amountPerUser <= token.balanceOf(address(this)),
+      "RewardAction::claim:Not enough token to claim"
+    );
+    token.transfer(msg.sender, Storage.amountPerUser);
+  }
+}
