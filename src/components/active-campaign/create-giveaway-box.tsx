1ec85a96f8de791afeafb30de849fb94fa238726
Christian Kobril <christiankobril@gmail.com>
2022-07-20 19:41:16 +0300

    chore: rename from giveaway to campaign

--- src/components/active-campaign/create-giveaway-box.tsx
+++ src/components/active-campaign/create-giveaway-box.tsx
@@ -5,7 +5,7 @@ export const CreateGiveawayBox = () => {
   return (
     <div className="h-72 rounded-2xl bg-elements grid place-content-center hover:cursor-pointer hover:opacity-80">
       <PlusBtn className="mx-auto my-3" />
-      <h5 className="text-center text-xl px-6 text-white">New Giveaway</h5>
+      <h5 className="text-center text-xl px-6 text-white">New Campaign</h5>
     </div>
   );
 };
